f5d0ec3cd9f89d9eafd86e60d46a5e19fedd4b78
soosung <soosung.lee@samsung.com>
2024-04-26 14:03:52 +0900

    add getZegop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello!");
         System.out.println("Hello!");
-        System.out.println("Hello!");
+
+        int a = 3;
+        System.out.println("제곱: "+ getZegop(a));
     }
+
+    private static int getZegop(int a) {
+        return a*a;
+    }
+
 }
